54ec89351a29045ce92484cfbf2f1c48c4313c8a
Dan Calise <calise@gmail.com>
2018-08-01 23:38:18 -0400

    fixing fb api key issue

--- src/config/agent.js
+++ src/config/agent.js
@@ -1,15 +1,14 @@
-import firebase from 'firebase';
-import 'firebase/app';
+import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import { FirebaseConfig } from './keys';
 
 firebase.initializeApp(FirebaseConfig);
 
-const databseRef = firebase.database().ref();
+// const databseRef = firebase.database().ref();
 
 const Auth = firebase.auth();
 
 export default firebase;
-export const todosRef = databseRef.child('todos');
+// export const todosRef = databseRef.child('todos');
 export { Auth };
--- src/config/dev.js
+++ src/config/dev.js
@@ -1,10 +1,9 @@
-const FirebaseConfig = {
-  apiKey: 'AIzaSyArI14BEQlODn34ZoSmA_xrkh4YajmkC-M',
-  authDomain: 'hockey-stats-bdd6e.firebaseapp.com',
-  databaseURL: 'https://hockey-stats-bdd6e.firebaseio.com',
-  projectId: 'hockey-stats-bdd6e',
-  storageBucket: 'hockey-stats-bdd6e.appspot.com',
-  messagingSenderId: '958185367239',
-};
-
-export default FirebaseConfig;
+/* eslint-disable */
+export const FirebaseConfig = {
+  apiKey: "AIzaSyArI14BEQlODn34ZoSmA_xrkh4YajmkC-M",
+  authDomain: "hockey-stats-bdd6e.firebaseapp.com",
+  databaseURL: "https://hockey-stats-bdd6e.firebaseio.com",
+  projectId: "hockey-stats-bdd6e",
+  storageBucket: "hockey-stats-bdd6e.appspot.com",
+  messagingSenderId: "958185367239"
+}
