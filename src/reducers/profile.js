7f5b106a06adaaefc1de928461336665fce204ff
Dan Calise <calise@gmail.com>
2018-09-29 16:21:25 -0400

    removing unused action

--- src/reducers/profile.js
+++ src/reducers/profile.js
@@ -2,8 +2,6 @@ export default (state = {}, action) => {
   switch (action.type) {
     case 'PROFILE_PAGE_UNLOADED':
       return {};
-    case 'UPDATE_FIELD_PROFILE':
-      return { ...state, [action.key]: action.value };
     case 'UPDATE_PROFILE':
       return { ...state};
     case 'GET_PROFILE':
